f388973cc2b017dd8304b61f367c54883fe57d7d
RebeccaFransson <rebecca@kollektiva.se>
2018-01-09 11:17:34 +0100

    /

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json('hey there')
+})
+
 app.listen(8080, () => {
     console.log('listening on port 8080')
 })
