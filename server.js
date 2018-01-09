78131082b3d37d2bdea66a1dc80977474e7003ad
RebeccaFransson <rebecca@kollektiva.se>
2018-01-09 11:27:04 +0100

    process port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/hey', (req, res) => {
     res.json('hey there')
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('listening on port 8080')
 })
